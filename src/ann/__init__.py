e175c10831c0d770a5915f51d61cc09cc85ba697
asanskar24 <asanskar24@gmail.com>
2026-03-05 16:29:49 +0530

    ch

--- src/ann/__init__.py
+++ src/ann/__init__.py
@@ -1,6 +1,6 @@
 # ANN Module - Neural Network Implementation
 from .neural_layer import Layer
-from .neural_network import MLP
+from .neural_network import NeuralNetwork
 from .activations import Activation
 from .optimizers import SGD, Momentum, NAG, RMSProp, Adam, Nadam, get_optimizer
 from .objective_functions import Loss
